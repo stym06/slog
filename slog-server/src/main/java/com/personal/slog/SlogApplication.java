1154cda52e3af09924b69991e78222e26f4b7f13
Satyam Raj <satyam.raj@Groww-Admins-MacBook-Pro.local>
2022-08-26 19:22:09 +0530

    init push

---
+++ slog-server/src/main/java/com/personal/slog/SlogApplication.java
@@ -0,0 +1,13 @@
+package com.personal.slog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SlogApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SlogApplication.class, args);
+	}
+
+}
---
+++ slog-server/src/test/java/com/personal/slog/SlogApplicationTests.java
@@ -0,0 +1,13 @@
+package com.personal.slog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SlogApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
